7adfeeec1dda28258a8120a2cdb4a8ce5df57d06
Loi Chyan <loichyan@foxmail.com>
2025-07-02 10:53:57 +0000

    docs: update README

--- lua/meow/config.lua
+++ lua/meow/config.lua
@@ -1,6 +1,7 @@
 ---@meta
 
 ---@class MeoOptions
+---Root spec(s) to load. Imports of other modules are usually specified here.
 ---@field specs? MeoSpecs
 ---Perform a few patches on MiniDeps so that all enabled plugins can be
 ---recognized correctly during updating or cleaning.
